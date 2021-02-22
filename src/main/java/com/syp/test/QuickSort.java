a4934a4020b64ce19f1ed389d3d9fa541b37e131
shiyuping <shiyp@ngarihealth.com>
2021-02-22 11:32:12 +0800

    sort

---
+++ src/main/java/com/syp/test/QuickSort.java
@@ -0,0 +1,59 @@
+package com.syp.test;
+
+import java.util.Arrays;
+
+/**
+ * created by shiyuping on 2021/2/20
+ * 快速排序
+ * 时间复杂度(O(nlog2n))
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        return quickSort(arr, 0, arr.length - 1);
+    }
+
+    private int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+        return arr;
+    }
+
+    /**
+     * 将所有比下标基准值大的元素放基准值右边
+     * 比下标基准值小的元素放基准值左边
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    private int partition(int[] arr, int left, int right) {
+        // 设定基准值（pivot）
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
--- src/main/java/com/syp/test/SortAlgorithm.java
+++ src/main/java/com/syp/test/SortAlgorithm.java
@@ -9,17 +9,29 @@ public class SortAlgorithm {
 
     public static void main(String[] args){
         int[] a = {1,3,2,7,9,4,6,8,5,11,12,13,14,15,16};
-        insertionSort(a);
+        selectionSort(a);
         System.out.println(Arrays.toString(a));
     }
 
 
-    //冒泡
-    public static void bubbleSort(int[] a){
-        
-        for (int i=0;i<a.length;i++){
+    /**
+     * 冒泡排序
+     * <p>
+     * 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+     * <p>
+     * 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+     * <p>
+     * 针对所有的元素重复以上的步骤，除了最后一个。
+     * <p>
+     * 持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较
+     *
+     * @param a
+     */
+    public static void bubbleSort(int[] a) {
+
+        for (int i = 0; i < a.length; i++) {
             boolean flag = true;
-            for (int j=0;j<a.length-i-1;j++){
+            for (int j = 0; j < a.length - i - 1; j++) {
                 int tmp;
                 if (a[j] > a[j+1]) {
                     tmp = a[j+1];
@@ -35,14 +47,22 @@ public class SortAlgorithm {
         }
     }
 
-    //插入
-    public static void insertionSort(int[] a){
+    /**
+     * 插入排序
+     * 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+     * <p>
+     * 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
+     * （如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+     *
+     * @param a
+     */
+    public static void insertionSort(int[] a) {
 
-        for (int i=1;i<a.length;i++){
+        for (int i = 1; i < a.length; i++) {
             int value = a[i];
             int j = i-1;
             //遍历插入位置
-            for(;j>=0;j--){
+            for (; j >= 0; j--) {
                 if (a[j]>value){
                     a[j+1]=a[j];//移动
                 }else {
@@ -51,7 +71,59 @@ public class SortAlgorithm {
             }
             a[j+1]= value;
         }
+    }
 
+    /**
+     * 选择排序
+     * 1.首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+     * <p>
+     * 2.再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+     * <p>
+     * 3.重复第二步，直到所有元素均排序完毕。
+     *
+     * @param arr
+     */
+    public static void selectionSort(int[] arr) {
+        // 总共要经过 N-1 轮比较
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            // 每轮需要比较的次数 N-i
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    // 记录目前能找到的最小值元素的下标
+                    min = j;
+                }
+            }
+            // 将找到的最小值和i位置所在的值进行交换
+            if (i != min) {
+                int tmp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = tmp;
+            }
+        }
+    }
 
+    /**
+     * 希尔排序
+     * 缩小增量排序
+     */
+    public static void shellSort(int[] arr) {
+        int gap = 1;
+        while (gap < arr.length / 3) {
+            gap = gap * 3 + 1;
+        }
+
+        while (gap > 0) {
+            for (int i = gap; i < arr.length; i++) {
+                int tmp = arr[i];
+                int j = i - gap;
+                while (j >= 0 && arr[j] > tmp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = tmp;
+            }
+            gap = (int) Math.floor(gap / 3);
+        }
     }
 }
